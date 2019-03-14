73017df01ee800e98fbc302cd1f9eb4673b9fedb
Slee, Dominic <dominic.slee@news.com.au>
2019-03-14 13:09:23 +1100

    Add bad errors...

--- MultiProcess.py
+++ MultiProcess.py
@@ -2,37 +2,48 @@ from tqdm import tqdm
 import multiprocessing
 
 MANAGER = multiprocessing.Manager()
-# TODO error handling
+
+class MultiProcessException(Exception):
+  pass
 
 class MultiProcess:
-  def __init__(self, loading_bar=True):
+  def __init__(self, show_loading_bar=True):
     self.pool = multiprocessing.Pool()
-    self.jobs = {}
-    self.job_count = 0
-    self.loading_bar = loading_bar
+    self.jobs = []
+    self.show_loading_bar = show_loading_bar
+    self.errQ = Queue()
 
-  def add_tasks(self, fn_name, arr_of_args):
+  def _reset(self):
+    while not self.errQ.empty():
+      self.errQ.pop()
+    self.jobs = []
+
+  def add_tasks(self, fn, arr_of_args):
     """add tasks to be done"""
-    if not 'fn_name' in self.jobs:
-      self.jobs[fn_name] = []
-    self.jobs[fn_name] += arr_of_args
-    self.job_count += len(arr_of_args)
+    self.jobs += [(fn, self.errQ) + args for args in arr_of_args]
 
   def do_tasks(self):
     """Block the thread and complete the tasks"""
-    if self.loading_bar:
-      pbar = tqdm(total=self.job_count)
+    job_count = len(self.jobs)
+    if self.show_loading_bar:
+      pbar = tqdm(total=job_count)
 
-    for job_fn, job_arr_of_args in self.jobs.items():
-      if not self.loading_bar:
-        self.pool.imap_unordered(job_fn, job_arr_of_args)
-      else:
-        for _ in enumerate(self.pool.imap_unordered(job_fn, job_arr_of_args)):
-          pbar.update(1)
+    if not self.show_loading_bar:
+      self.pool.imap_unordered(my_worker, self.jobs)
+    else:
+      for _ in enumerate(self.pool.imap_unordered(my_worker, self.jobs)):
+        pbar.update(1)
     
     pbar.close()
-    self.jobs = {}
-    self.job_count = 0
+
+    # Raise exceptions, if there were any
+    if not self.errQ.empty():
+      exceptions = []
+      while not self.errQ.empty():
+        exceptions.append(self.errQ.pop())
+      raise MultiProcessException('%s errors occurred:\n' % len(exceptions) + "\n".join(['ERROR: ' + str(e) for e in exceptions]))
+    
+    self._reset() 
   
   def close(self):
     self.pool.close()
