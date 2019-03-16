3355bad2ec6b1c6b70a4b3197dee89b4ca36f8c2
Dom Slee <domslee97@gmail.com>
2019-03-17 02:01:56 +1100

    Add linters

--- MultiProcess.py
+++ MultiProcess.py
@@ -1,31 +1,33 @@
-from tqdm import tqdm
 import multiprocessing
 import traceback
 import dill
+from tqdm import tqdm
 
 MANAGER = multiprocessing.Manager()
 
+
 class MultiProcessException(Exception):
   pass
+
 
 class MultiProcess:
   def __init__(self, show_loading_bar=True):
     self.pool = multiprocessing.Pool()
     self.jobs = []
     self.show_loading_bar = show_loading_bar
-    self.errQ = Queue()
+    self.err_q = Queue()
     self.alive = True
 
   def _reset(self):
-    while not self.errQ.empty():
-      self.errQ.pop()
+    while not self.err_q.empty():
+      self.err_q.pop()
     self.jobs = []
 
   def add_tasks(self, fn, arr_of_args):
     """add tasks to be done"""
     if not self.alive:
       raise MultiProcessException("CLOSED, refusing to add tasks")
-    arr = [dill.dumps((fn, self.errQ) + (args)) for args in arr_of_args]
+    arr = [dill.dumps((fn, self.err_q) + (args)) for args in arr_of_args]
     self.jobs += arr
 
   def do_tasks(self, called_from_close=False):
@@ -33,8 +35,7 @@ class MultiProcess:
     if not self.alive:
       if called_from_close:
         return
-      else:
-        raise MultiProcessException('CLOSED, refusing to do tasks')
+      raise MultiProcessException('CLOSED, refusing to do tasks')
     job_count = len(self.jobs)
     if self.show_loading_bar:
       pbar = tqdm(total=job_count)
@@ -45,20 +46,22 @@ class MultiProcess:
       for _ in enumerate(self.pool.imap_unordered(my_worker, self.jobs)):
         self._check_for_exceptions()
         pbar.update(1)
-    
+
     # Raise exceptions, if there were any
     self._check_for_exceptions()
     pbar.close()
-    self._reset() 
-  
+    self._reset()
+
   def _check_for_exceptions(self):
-    if not self.errQ.empty():
+    if not self.err_q.empty():
       exceptions = []
-      while not self.errQ.empty():
-        exceptions.append(self.errQ.pop())
+      while not self.err_q.empty():
+        exceptions.append(self.err_q.pop())
       self._reset()
       self.close()
-      raise MultiProcessException('%s errors occurred:\n' % len(exceptions) + "\n".join(['ERROR: ' + str(e) for e in exceptions]))
+      raise MultiProcessException(
+          '%s errors occurred:\n' % len(exceptions) +
+          "\n".join(['ERROR: ' + str(e) for e in exceptions]))
 
   def close(self):
     self.do_tasks(True)
@@ -69,37 +72,39 @@ class MultiProcess:
 def my_worker(args):
   args = dill.loads(args)
   fn = args[0]
-  errQ = args[1]
-  remArgs = args[2:]
+  err_q = args[1]
+  rem_args = args[2:]
   try:
-    fn(*remArgs)
+    fn(*rem_args)
   except Exception:
-    errQ.push('Error in function call "%s(%s)"\n%s' % (fn.__name__, remArgs, traceback.format_exc()))
+    err_q.push('Error in function call "%s(%s)"\n%s' %
+               (fn.__name__, rem_args, traceback.format_exc()))
 
 
 """A lightweight wrapper for multiprocess.manager.Queue()"""
+
+
 class Queue:
   def __init__(self):
     self.q = MANAGER.Queue()
     #for method in dir(self.q):
     #  if method[0] != '_':
     #    setattr(self, method, getattr(self.q, method]))
-  
+
   def get(self):
     return self.q.get()
-  
+
   def put(self, v):
     self.q.put(v)
 
   def pop(self):
     return self.get()
-  
+
   def push(self, v):
     self.q.put(v)
-  
+
   def empty(self):
     return self.q.empty()
-  
+
   def qsize(self):
     return self.q.qsize()
-  
