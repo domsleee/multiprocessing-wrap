bb2f06cff613ce9ade5810be5b82776fccd760eb
Dom Slee <domslee97@gmail.com>
2019-03-17 18:00:22 +1100

    Add pipfile and long description

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="multiprocess",
     version="0.0.1",
     description="A better way to run shell commands in Python.",
     author='Dominic Slee',
     author_email='domslee1@gmail.com',
-    #long_description='TODO',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     py_modules=[
         'multiprocess'
     ],
